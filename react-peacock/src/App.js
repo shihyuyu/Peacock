e065207577922b77d6b4c1876eb16b6fad6d43e0
Yuti Shih <yutishih@gmail.com>
2021-11-03 15:53:53 -0400

    Installed React Installed Bootstrap 5 created 1st component "Navbar.js"

---
+++ react-peacock/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <body>
+        <Navbar />
+      </body>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-peacock/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                Meow Hello
+            </div>
+        )
+    }
+}
+
+export default Navbar
+
